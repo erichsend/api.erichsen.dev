982efe249ce557acc5db9cd639db6a4411eb0fdc
David Erichsen <dave.erichsen@gmail.com>
2025-03-25 11:06:25 -0500

    feat: add omeka

--- omeka/docker/entrypoint.sh
+++ omeka/docker/entrypoint.sh
@@ -6,24 +6,28 @@ set -e
 : "${S3_EXPIRATION:=10}"
 : "${S3_FORCE_SSL:=1}"
 
-cat >/var/www/html/config.ini <<EOF
-; Omeka Configuration File
-
+cat >/var/www/html/db.ini <<EOF
 [database]
-host     = ${DB_HOST}
-username = ${DB_USER}
-password = ${DB_PASS}
-dbname   = ${DB_NAME}
-port     = ${DB_PORT}
+host     = "${DB_HOST}"
+username = "${DB_USER}"
+password = "${DB_PASS}"
+dbname   = "${DB_NAME}"
+port     = "${DB_PORT}"
 
+EOF
+
+cat >/var/www/html/application/config/config.ini <<EOF
+[site]
 [storage]
-adapter                           = "Omeka_Storage_Adapter_ZendS3"
-adapterOptions.accessKeyId        = "${S3_KEY}"
-adapterOptions.secretAccessKey    = "${S3_SECRET}"
-adapterOptions.bucket             = "${S3_BUCKET}"
-adapterOptions.endpoint           = "${S3_ENDPOINT}"
-adapterOptions.expiration         = "${S3_EXPIRATION}"
-adapterOptions.forceSSL           = "${S3_FORCE_SSL}"
+storage.adapter                           = "Omeka_Storage_Adapter_ZendS3"
+storage.adapterOptions.accessKeyId        = "${S3_KEY}"
+storage.adapterOptions.secretAccessKey    = "${S3_SECRET}"
+storage.adapterOptions.bucket             = "${S3_BUCKET}"
+storage.adapterOptions.endpoint           = "${S3_ENDPOINT}"
+storage.adapterOptions.expiration         = "${S3_EXPIRATION}"
+storage.adapterOptions.forceSSL           = "${S3_FORCE_SSL}"
+[log]
+[debug]
 EOF
 
 # Start Apache in the foreground
